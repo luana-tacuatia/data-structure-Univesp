79b6139a11820deb0b53e9a8913eecebb6f7a63f
luana-tacuatia <tacuatia@yahoo.com.br>
2023-08-09 01:10:28 -0300

    updating files.

---
+++ week1/aula1_sem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout <<"Hello World!"<< std::endl;
+    return 0;
+}
+
+// comands to run c++ code:
+// g++ aula1_sem1.cpp -o aula1_sem1
+// result: a.exe*  aula1_sem1.cpp  aula1_sem1.exe*  README.md
+// now run: ./aula1_sem1.exe
